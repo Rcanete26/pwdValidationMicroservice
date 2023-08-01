cda9cd4ce282f35fd3379bc2111299cb7fa2f8fb
Ryan Alexander Canete <101620410+Rcanete26@users.noreply.github.com>
2023-07-31 20:06:10 -0700

    Update client.py

--- pwdValidation v1/client.py
+++ pwdValidation v1/client.py
@@ -15,22 +15,9 @@ example_send = {
 'symbol': True
 }
 # example_send={}
-message = client.recv(1024)
-print(message.decode("utf-8"))
-file = pickle.dumps({})
-client.sendall(file)
-validation_result = client.recv(1024)
-print(validation_result.decode("utf-8"))
-
-
-example_send = {
-'password': 'Password1234ABCSTRE!',
-'upper_length': 20,
-'lower_length': 0,
-'digit': True,
-'uppercase': True, 
-'lowercase': True, 
-'symbol': True
-}
-file = pickle.dumps({})
-client.sendall(file)
+message = client.recv(1024)  # recieve the connetion message
+print(message.decode("utf-8"))  # decode and print connection message
+file = pickle.dumps({})  # encode dictionary + characteristics to validate
+client.sendall(file)      # send data
+validation_result = client.recv(1024)    # reviece result of validation from microservice
+print(validation_result.decode("utf-8"))  # print result of validation
